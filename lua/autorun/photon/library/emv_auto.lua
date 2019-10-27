f37ede15de872dab9453b656482fa6cd92eddfa1
John Internet <jonjon1234.github@gmail.com>
2019-10-27 10:39:09 +0000

    Fixed an issue where auto components would register with no modes.

--- lua/autorun/photon/library/emv_auto.lua
+++ lua/autorun/photon/library/emv_auto.lua
@@ -5,6 +5,19 @@ if not EMVU.AutoIndex then EMVU.AutoIndex = {} end
 
 
 function EMVU:AddAutoComponent( component, name )
+	-- temporary debug
+	-- local info = debug.getinfo(2, 'S')
+	-- if not info.short_src:StartWith("addons/photon/lua/autorun/photon/library/") then
+	-- 	ErrorNoHalt("Loading component from " .. info.source)
+	-- 	print()
+	-- 	return
+	-- end
+
+	if not component.Modes then
+		ErrorNoHalt(Format("Component %s is missing its Modes field.\n", name))
+		return
+	end
+
 	EMVU.Auto[ name ] = component
 end
 
@@ -19,6 +32,6 @@ function EMVU:PrecacheAutoModels()
 		util.PrecacheModel( prop.Model )
 	end
 end
-hook.Add( "Initialize", "Photon.PrecacheAutoModels", function() 
+hook.Add( "Initialize", "Photon.PrecacheAutoModels", function()
 	EMVU:PrecacheAutoModels()
 end)
