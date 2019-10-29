7bdcfd7aaae7a9bd24eb9290bd80b696e81db3eb
John Internet <jonjon1234.github@gmail.com>
2019-10-29 13:28:27 +0000

    Added sound player functions.

--- lua/autorun/photon/library/emv_sounds.lua
+++ lua/autorun/photon/library/emv_sounds.lua
@@ -14,3 +14,29 @@ EMVU.Sounds.SignalOn = "emv/ui/emv_signal_on.wav"
 EMVU.Sounds.SignalOff = "emv/ui/emv_signal_off.wav"
 EMVU.Sounds.Down = "emv/ui/emv_chirp_down.wav"
 EMVU.Sounds.Up = "emv/ui/emv_chirp_up.wav"
+
+--- Play a toggling sound.
+-- @bool state Current sound state.
+-- @string a On state sound.
+-- @string b Off state sound.
+function EMVU.Sounds.Sound(state, a, b)
+	surface.PlaySound(state and a or b)
+end
+
+--- Play the tocks.
+-- @bool state Current sound state.
+function EMVU.Sounds:Tocks(state)
+	self:Sound(state, self.Tick, self.Tock)
+end
+
+--- Play signalling sound.
+-- @bool state Current sound state.
+function EMVU.Sounds:Signal(state)
+	self:Sound(state, self.SignalOn, self.SignalOff)
+end
+
+--- Play panel button sound.
+-- @bool state Current sound state.
+function EMVU.Sounds:Panel(state)
+	self:Sound(state, self.Down, self.Up)
+end
