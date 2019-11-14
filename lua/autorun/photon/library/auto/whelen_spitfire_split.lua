5e9eb9eebc7556da7f2716258dcf3caf9cc435c8
SuperMighty1 <supermighty1@gmail.com>
2019-11-14 19:32:25 +0000

    Whelen Spitfire

---
+++ lua/autorun/photon/library/auto/whelen_spitfire_split.lua
@@ -0,0 +1,101 @@
+AddCSLuaFile()
+
+local A = "AMBER"
+local R = "RED"
+local DR = "D_RED"
+local B = "BLUE"
+local W = "WHITE"
+local CW = "C_WHITE"
+local SW = "S_WHITE"
+local G = "GREEN"
+local RB = "BLUE/RED"
+
+local name = "Whelen Spitfire Split"
+
+local COMPONENT = {}
+
+COMPONENT.Model = "models/supermighty/photon/whelen_spitfire.mdl"
+COMPONENT.Skin = 0
+COMPONENT.Bodygroups = {}
+COMPONENT.NotLegacy = true
+COMPONENT.ColorInput = 1
+COMPONENT.UsePhases = true
+COMPONENT.Category = "Exterior"
+COMPONENT.DefaultColors = {
+	[1] = "RED",
+	[2] = "BLUE"
+}
+
+COMPONENT.Meta = {
+	auto_ion_left = {
+		AngleOffset = 0,
+		W = 6.9,
+		H = 6.9,
+		WMult = 1,
+		Sprite = "sprites/emv/whelen_ion_left",
+		Scale = 1,
+		NoLegacy = true,
+		DirAxis = "Up",
+		DirOffset = 90
+	},
+	auto_ion_right = {
+		AngleOffset = 0,
+		W = 6.9,
+		H = 6.9,
+		WMult = 1,
+		Sprite = "sprites/emv/whelen_ion_right",
+		Scale = 1,
+		NoLegacy = true,
+		DirAxis = "Up",
+		DirOffset = 90
+	},
+}
+
+COMPONENT.Positions = {
+
+	[1] = { Vector( 1.46, 0.5, 0 ), Angle( 0, 0, 0 ), "auto_ion_left" },
+	[2] = { Vector( -1.46, 0.5, 0 ), Angle( 0, 0, 0 ), "auto_ion_right" },
+
+}
+
+COMPONENT.Sections = {
+	["auto_ion"] = {
+		[1] = { { 1, "_1" }, { 2, "_2" } },
+		[2] = { { 1, "_1" } },
+		[3] = { { 2, "_2" } }
+	},
+}
+
+COMPONENT.Patterns = {
+	["auto_ion"] = {
+		["code1"] = { 2, 0, 2, 0, 2, 2, 2, 0, 3, 0, 3, 0, 3, 3, 3, 0 },
+		["code2"] = { 
+			0, 
+			3, 0, 3, 0,
+			2, 0, 2, 0,
+			3, 0, 3, 0,
+			2, 0, 2, 0,
+			3, 0, 3, 0,
+			2, 0, 2, 0,
+			3, 3, 3, 0, 2, 2, 2,
+			3, 3, 3, 0, 2, 2, 2,
+			3, 3, 3, 0, 2, 2, 2,
+			3, 3, 0, 2, 2,
+			3, 3, 0, 2, 2,
+			3, 3, 0, 2, 2,
+			3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 
+		}
+	}
+}
+
+COMPONENT.Modes = {
+	Primary = {
+		M1 = { ["auto_ion"] = "code1", },
+		M2 = { ["auto_ion"] = "code2", },
+		M3 = { ["auto_ion"] = "code2", }
+	},
+	Auxiliary = {},
+	Illumination = {}
+}
+
+EMVU:AddAutoComponent( COMPONENT, name )
