86e73c2b1c311903dad98d874f7c4e9ae83e4975
John Internet <jonjon1234.github@gmail.com>
2019-11-28 21:50:22 +0000

    photon_express_edit callback.

--- lua/autorun/photon/cl_photon_settings.lua
+++ lua/autorun/photon/cl_photon_settings.lua
@@ -44,4 +44,4 @@ CreateClientConVar("photon_christmas_mode_auto", "1", true)
 CreateClientConVar("photon_comp_editor_scale", "1", true)
 CreateClientConVar("photon_express_edit", 0, false)
 
-cvars.AddChangeCallback("photon_express_edit",function (name, old, val) PHOTON_EXPRESS = val end)
+cvars.AddChangeCallback("photon_express_edit", function(name, old, new) PHOTON_EXPRESS = new end)
