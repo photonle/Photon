57ce10a4a9063c0dedb642a5f8b236261a4631d0
John Internet <jonjon1234.github@gmail.com>
2019-12-04 11:17:18 +0000

    Time to start fucking with heads.

---
+++ lua/autorun/photon/sh_functional.lua
@@ -0,0 +1,29 @@
+--[[-- Functional Programming / Currying Library.
+@copyright Photon Team
+@release v73 Grand Lake
+@author Joshua Piper
+@module functional
+--]]--
+module("functional")
+
+AddCSLuaFile()
+
+--- Builds a partial function, with stored arguments.
+-- @usage local positiveOnly = compose(max, 0)
+-- @usage print(positiveOnly(-1)) -- Returns 0.
+-- @usage print(positiveOnly(2)) -- Returns 2.
+-- @tparam function func Input function to curry.
+-- @tparam ... vararg Arguments to store.
+function compose(func, ...)
+	local args = {...}
+	local st = #args
+
+	return function(...)
+		local m = select("#", ...)
+		for i = 1, m do
+			args[st + i] = select(i, ...)
+		end
+
+		callable(unpack(args, 1, st + m))
+	end
+end
