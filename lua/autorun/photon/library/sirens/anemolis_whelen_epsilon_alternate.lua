329adc1738ce0dec733f010b95b6690d2a317120
DatedGore <48846216+DatedGore@users.noreply.github.com>
2019-12-26 19:04:50 -0500

    Added Whelen Epsilon Alternate siren.

---
+++ lua/autorun/photon/library/sirens/anemolis_whelen_epsilon_alternate.lua
@@ -0,0 +1,14 @@
+---Made by: anemolis72
+if not EMVU then return end
+
+	EMVU.AddCustomSiren("whelen_epsilon_alternate", {
+	Name = "Epsilon Alternate",
+	Category = "Whelen",
+Set = {
+		{Name = "WAIL", Sound = "emv/sirens/whelen epsilon alternate/emv_wail.wav", Icon="wail"},
+		{Name = "YELP", Sound = "emv/sirens/whelen epsilon alternate/emv_yelp.wav", Icon="yelp"},
+		{Name = "PHASER", Sound = "emv/sirens/whelen epsilon alternate/emv_phaser.wav", Icon="phaser"},
+	},
+	Horn = "emv/sirens/whelen epsilon alternate/emv_horn.wav",
+	Volume = "100"
+})
