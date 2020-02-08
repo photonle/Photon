9402fce08a2dfc6f581cf11335f5598d1fadfd60
DatedGore <48846216+DatedGore@users.noreply.github.com>
2020-02-08 16:01:13 -0500

    Update anemolis_chp_bike.lua

--- lua/autorun/photon/library/sirens/anemolis_chp_bike.lua
+++ lua/autorun/photon/library/sirens/anemolis_chp_bike.lua
@@ -7,7 +7,7 @@ EMVU.AddCustomSiren("chp_bike", {
 	Set = {
 			{Name = "WAIL", Sound = "emv/sirens/chp bike/emv_wail.wav", Icon="wail"},
 			{Name = "YELP", Sound = "emv/sirens/chp bike/emv_yelp.wav", Icon="yelp"},
-			},
+	},
 	Horn = "emv/sirens/chp bike/emv_horn.wav",
 	Volume = 100
 })
