e3356215c972c26663caefe2d28e3c7ea781e38a
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-12-31 20:10:19 +0000

    Revert change for amber domes.

--- lua/autorun/photon/library/auto/dome_amber.lua
+++ lua/autorun/photon/library/auto/dome_amber.lua
@@ -5,7 +5,48 @@ local name = "Dome Light Amber"
 
 local COMPONENT = {}
 
+COMPONENT.Model = "models/schmal/dome_light.mdl"
 COMPONENT.Skin = 0
+COMPONENT.Bodygroups = {}
+COMPONENT.NotLegacy = true
+COMPONENT.ForwardTranslation = true
+COMPONENT.UsePhases = true
+COMPONENT.Category = "Exterior"
+
+COMPONENT.Meta = {
+	auto_dome_a = {
+		NoLegacy = true,
+		AngleOffset = "R",
+		DirAxis = "Up",
+		DirOffset = -90,
+		Speed = 10,
+		W = 6,
+		H = 8,
+		Sprite = "sprites/emv/visionslr",
+		Scale = 2.75,
+		WMult = 1,
+	},
+	auto_dome_b = {
+		NoLegacy = true,
+		AngleOffset = "R",
+		DirAxis = "Up",
+		DirOffset = 90,
+		Speed = -10,
+		W = 6,
+		H = 8,
+		Sprite = "sprites/emv/visionslr",
+		Scale = 2.75,
+		WMult = 1,
+	},
+}
+
+COMPONENT.Positions = {
+
+	[1] = { Vector( 0, 0, 2.5 ), Angle( 0, -90, 0 ), "auto_dome_a" },
+	[2] = { Vector( 0, 0, 2.5 ), Angle( 0, -90, 0 ), "auto_dome_b" },
+
+}
+
 COMPONENT.Sections = {
 	["auto_light_dome"] = {
 		[1] = { { 1, A } },
@@ -13,4 +54,22 @@ COMPONENT.Sections = {
 	},
 }
 
-EMVU:AddAutoComponent( COMPONENT, name, "Dome Light Blue" )
+COMPONENT.Patterns = {
+	["auto_light_dome"] = {
+		["mode1"] = { 1 },
+		["mode1A"] = { 1 },
+		["mode1B"] = { 2 },
+	}
+}
+
+COMPONENT.Modes = {
+	Primary = {
+		M1 = { ["auto_light_dome"] = "mode1", },
+		M2 = { ["auto_light_dome"] = "mode1", },
+		M3 = { ["auto_light_dome"] = "mode1", }
+	},
+	Auxiliary = {},
+	Illumination = {}
+}
+
+EMVU:AddAutoComponent( COMPONENT, name )
