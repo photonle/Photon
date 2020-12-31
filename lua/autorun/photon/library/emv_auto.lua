71f2799bb4719a4cd9ed6d98158c22fa57b5a5b5
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-12-31 16:31:24 +0000

    Add loader for deferred things.

--- lua/autorun/photon/library/emv_auto.lua
+++ lua/autorun/photon/library/emv_auto.lua
@@ -33,6 +33,34 @@ local autoFiles = file.Find( "autorun/photon/library/auto/*", "LUA" )
 for _,_file in pairs( autoFiles ) do
 	include( "auto/" .. _file )
 end
+local lastFound = -1
+local found = 0
+local iter = 0
+while found ~= lastFound do
+	iter = iter + 1
+	lastFound = found
+	found = 0
+
+	print(("iteration %s"):format(iter))
+	for name, data in pairs(EMVU.AutoStaging) do
+		found = found + 1
+
+		local base, component = unpack(data)
+		print(("\t%s => %s"):format(base, name))
+		if EMVU.Auto[base] then
+			EMVU.Auto[name] = table.Inherit(component, EMVU.Auto[base])
+			EMVU.AutoStaging[name] = nil
+		end
+	end
+end
+if found ~= 0 then
+	PhotonWarning("Attempted to load inherited components, but the base component wasn't loaded!")
+	for name, data in pairs(EMVU.AutoStaging) do
+		local base, _ = unpack(data)
+		PhotonWarning(("\t%s attempted to load from %s, but %s wasn't found."):format(name, base, base))
+	end
+end
+
 
 function EMVU:PrecacheAutoModels()
 	for id,prop in pairs( EMVU.Auto ) do
