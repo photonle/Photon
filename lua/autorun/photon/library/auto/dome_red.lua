2ee41fb7edfca5b56a2ed05265839f8ec9a41acc
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-12-31 20:12:39 +0000

    Convert the dome lights to use inheretence.

--- lua/autorun/photon/library/auto/dome_red.lua
+++ lua/autorun/photon/library/auto/dome_red.lua
@@ -1,53 +1,11 @@
 AddCSLuaFile()
 
 local R = "RED"
-
 local name = "Dome Light Red"
 
 local COMPONENT = {}
-
-COMPONENT.Model = "models/schmal/dome_light.mdl"
+COMPONENT.Base = "Dome Light Amber"
 COMPONENT.Skin = 1
-COMPONENT.Bodygroups = {}
-COMPONENT.NotLegacy = true
-COMPONENT.ForwardTranslation = true
-COMPONENT.UsePhases = true
-COMPONENT.Category = "Exterior"
-
-COMPONENT.Meta = {
-	auto_dome_a = {
-		NoLegacy = true,
-		AngleOffset = "R",
-		DirAxis = "Up",
-		DirOffset = -90,
-		Speed = 10,
-		W = 6,
-		H = 8,
-		Sprite = "sprites/emv/visionslr",
-		Scale = 2.75,
-		WMult = 1,
-	},
-	auto_dome_b = {
-		NoLegacy = true,
-		AngleOffset = "R",
-		DirAxis = "Up",
-		DirOffset = 90,
-		Speed = -10,
-		W = 6,
-		H = 8,
-		Sprite = "sprites/emv/visionslr",
-		Scale = 2.75,
-		WMult = 1,
-	},
-}
-
-COMPONENT.Positions = {
-
-	[1] = { Vector( 0, 0, 2.5 ), Angle( 0, -90, 0 ), "auto_dome_a" },
-	[2] = { Vector( 0, 0, 2.5 ), Angle( 0, -90, 0 ), "auto_dome_b" },
-
-}
-
 COMPONENT.Sections = {
 	["auto_light_dome"] = {
 		[1] = { { 1, R } },
@@ -55,22 +13,4 @@ COMPONENT.Sections = {
 	},
 }
 
-COMPONENT.Patterns = {
-	["auto_light_dome"] = {
-		["mode1"] = { 1 },
-		["mode1A"] = { 1 },
-		["mode1B"] = { 2 },
-	}
-}
-
-COMPONENT.Modes = {
-	Primary = {
-		M1 = { ["auto_light_dome"] = "mode1", },
-		M2 = { ["auto_light_dome"] = "mode1", },
-		M3 = { ["auto_light_dome"] = "mode1", }
-	},
-	Auxiliary = {},
-	Illumination = {}
-}
-
-EMVU:AddAutoComponent( COMPONENT, name )
+EMVU:AddAutoComponent(COMPONENT, name)
