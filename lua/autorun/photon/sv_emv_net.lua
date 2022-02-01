d0ca330507187065126b4ed26f6445aa1ebdc789
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2022-02-01 12:15:56 +0000

    fix: Signal Net now sets CurrentSignal.

--- lua/autorun/photon/sv_emv_net.lua
+++ lua/autorun/photon/sv_emv_net.lua
@@ -221,9 +221,13 @@ function Photon.Net:Signal( ply )
 	if not ply:InVehicle() or not ply:GetVehicle():Photon() then return end
 	local car = ply:GetVehicle()
 	local signal = net.ReadInt(3)
-	if not signal == CAR_TURNING_LEFT or not signal == CAR_TURNING_RIGHT or not signal == CAR_HAZARD then return false end
-	if signal == car:CAR_Signal() then car:CAR_StopSignals() return end
-	car:CAR_Signal( signal )
+
+	if signal < CAR_BLINKER_NONE or signal > CAR_BLINKER_HAZARD then return false end
+	if signal == car:Photon_Signal() then
+		return car:Photon_SignalStop()
+	end
+
+	car:SetPhotonNet_CurrentSignal(signal)
 end
 net.Receive("photon_signal", function( l,p )
 	Photon.Net:Signal( p )
