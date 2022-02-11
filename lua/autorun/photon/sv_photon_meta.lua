192a14c1a83f240e97ed418772a6ff853b922abc
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2022-02-11 16:47:49 +0000

    style: Cleanup sv meta.

--- lua/autorun/photon/sv_photon_meta.lua
+++ lua/autorun/photon/sv_photon_meta.lua
@@ -1,13 +1,13 @@
 
-function Photon:SetupCar( ent, index )
-	ent:SetPhotonNet_Headlights( false )
-	ent:SetPhotonNet_Braking( false )
-	ent:SetPhotonNet_Running( false )
-	ent:SetPhotonNet_Reversing( false )
+function Photon:SetupCar(ent, index)
+	ent:SetPhotonNet_Headlights(false)
+	ent:SetPhotonNet_Braking(false)
+	ent:SetPhotonNet_Running(false)
+	ent:SetPhotonNet_Reversing(false)
 	ent:Photon_SignalStop()
 
 	if ent:Photon_WheelEnabled() then
-		ent:Photon_SetWheelIndex( 1 )
+		ent:Photon_SetWheelIndex(1)
 	end
 
 	-- ent:SetNWString( "PhotonVehicle", index )
