2584009d95102f1793a9fee86d7d761122b0806d
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-04-02 01:43:12 +0100

    feat: Add PhotonDebu.

--- lua/autorun/photon/sh_photon_init.lua
+++ lua/autorun/photon/sh_photon_init.lua
@@ -49,6 +49,7 @@ end
 
 Photon.Messages.Colours.Error = Color(255, 0, 0)
 Photon.Messages.Colours.Warning = Color(255, 200, 0)
+Photon.Messages.Colours.Debug = Color(160, 160, 160)
 
 --- Print a message to console.
 -- @tparam vararg ... Inputs to display.
@@ -82,6 +83,7 @@ end
 
 Photon.Messages:BuildLevel("Error")
 Photon.Messages:BuildLevel("Warning")
+Photon.Messages:BuildLevel("Debug")
 
 AddCSLuaFile("cl_photon_eng.lua")
 AddCSLuaFile("cl_photon_meta.lua")
