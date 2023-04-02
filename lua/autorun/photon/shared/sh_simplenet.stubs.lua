842ef6517bbfed7e0cff2d099bef637425ce0f33
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-04-02 01:25:02 +0100

    docs: Add more functions to the stubs file.

--- lua/autorun/photon/shared/sh_simplenet.stubs.lua
+++ lua/autorun/photon/shared/sh_simplenet.stubs.lua
@@ -9,3 +9,66 @@
 -- @function Vehicle:GetPhotonNet_CurrentSignal(default)
 -- @param default Default value if no local value is found.
 -- @rint Current Signal Value.
+
+---
+-- @function Vehicle:SetPhotonNet_Blinker(value)
+-- @int value New Current Signal Value.
+
+---
+-- @function Vehicle:GetPhotonNet_Blinker(default)
+-- @param default Default value if no local value is found.
+-- @rint Current Signal Value.
+
+---
+-- @function Vehicle:SetPhotonNet_Headlights(value)
+-- @bool value New Current Signal Value.
+
+---
+-- @function Vehicle:GetPhotonNet_Headlights(default)
+-- @param default Default value if no local value is found.
+-- @rbool Current Signal Value.
+
+---
+-- @function Vehicle:SetPhotonNet_Breaking(value)
+-- @bool value New Current Signal Value.
+
+---
+-- @function Vehicle:GetPhotonNet_Breaking(default)
+-- @param default Default value if no local value is found.
+-- @rbool Current Signal Value.
+
+---
+-- @function Vehicle:SetPhotonNet_Reversing(value)
+-- @bool value New Current Signal Value.
+
+---
+-- @function Vehicle:GetPhotonNet_Reversing(default)
+-- @param default Default value if no local value is found.
+-- @rbool Current Signal Value.
+
+---
+-- @function Vehicle:SetPhotonNet_Running(value)
+-- @bool value New Current Signal Value.
+
+---
+-- @function Vehicle:GetPhotonNet_Running(default)
+-- @param default Default value if no local value is found.
+-- @rbool Current Signal Value.
+
+---
+-- @function Vehicle:SetPhotonNet_LEStayOn(value)
+-- @bool value New Current Signal Value.
+
+---
+-- @function Vehicle:GetPhotonNet_LEStayOn(default)
+-- @param default Default value if no local value is found.
+-- @rbool Current Signal Value.
+
+---
+-- @function Vehicle:SetPhotonNet_WheelIndex(value)
+-- @bool value New Current Signal Value.
+
+---
+-- @function Vehicle:GetPhotonNet_WheelIndex(default)
+-- @param default Default value if no local value is found.
+-- @rbool Current Signal Value.
