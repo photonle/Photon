138c3f54776108797907d41d2b5e900f68094478
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-04-02 01:17:19 +0100

    feat: Add simplified networking stubs.

---
+++ lua/autorun/photon/shared/_simplenet.stubs.lua
@@ -0,0 +1,11 @@
+---
+-- @classmod Vehicle
+
+---
+-- @function Vehicle:SetPhotonNet_CurrentSignal(value)
+-- @int value New Current Signal Value.
+
+---
+-- @function Vehicle:GetPhotonNet_CurrentSignal(default)
+-- @param default Default value if no local value is found.
+-- @rint Current Signal Value.
