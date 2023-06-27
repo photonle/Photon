b57f007d1464259b3c01d4091b3a73c1e989e0f0
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-06-27 20:56:31 +0100

    refacor: Use the resolver.

--- lua/autorun/photon/sh_emv_vehicles.lua
+++ lua/autorun/photon/sh_emv_vehicles.lua
@@ -853,56 +853,7 @@ function EMVU:CalculateAuto( name, data, autoInsert )
 		for id=1,#component.Positions do
 			local posData = component.Positions[ id ]
 			if isvector( posData[1] ) then
-				local newPos, newAng = Vector(), Angle()
-				if component.LegacyPositioning then
-					newPos:Set( posData[1] )
-					newPos:Rotate( adjustAng )
-					newPos:Mul( autoScale )
-					newPos:Add( autoPos )
-
-					if not component.NotLegacy then
-						newAng.y = newAng.y + 90
-						newAng:Set( posData[2] )
-						newAng:RotateAroundAxis( autoAng:Right(), -1*autoAng.p )
-						newAng:RotateAroundAxis( autoAng:Up(), -1*autoAng.r )
-					else
-						newAng:Set( posData[2] )
-						if component.ForwardTranslation then
-							newAng.p = newAng.p + (autoAng.r *-1)
-							newAng.y = newAng.y + autoAng.y
-							newAng.r = newAng.r + autoAng.p
-						else
-							newAng.p = newAng.p + autoAng.p
-							newAng.y = newAng.y + autoAng.y
-							newAng.r = newAng.r + autoAng.r
-						end
-					end
-				else
-					local componentMatrix = Matrix()
-					componentMatrix:Translate(autoPos)
-					componentMatrix:Rotate(autoAng)
-					if not component.NotLegacy then
-						componentMatrix:Rotate(Angle(0, -90, 0))
-					end
-
-					local autoScaleVector = isvector(autoScale) and (autoScale) or Vector(autoScale, autoScale, autoScale)
-
-					local offsetMatrix = Matrix()
-					offsetMatrix:Translate(posData[1] * autoScaleVector)
-					local schmAngle = posData[2]
-					if component.ForwardTranslation then
-						schmAngle = Angle(
-							-schmAngle.r,
-							schmAngle.y,
-							schmAngle.p
-						)
-					end
-					offsetMatrix:Rotate(schmAngle)
-					local out = componentMatrix * offsetMatrix
-
-					newPos = out:GetTranslation()
-					newAng = out:GetAngles()
-				end
+				local newPos, newAng = EMVU.Helper:ResolvePosition(component, posData[1], posData[2], autoPos, autoAng, autoScale)
 
 				EMVU.Positions[ name ][ offset + id ] = {
 					newPos, newAng, tostring( posData[3] .. "_" .. i ), posData[4] or false
